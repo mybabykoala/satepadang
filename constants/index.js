13b6c88117abd34379bad051d9530105d14b2ac0
mybabykoala <134411149+mybabykoala@users.noreply.github.com>
2023-08-06 02:14:40 +0700

    Update index.js
    update contract

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x43b646a6dc736b8759ce7e74d4ed9bf011b1b069";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
